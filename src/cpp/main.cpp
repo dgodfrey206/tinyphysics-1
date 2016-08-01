a11ab6bf62e22dbb3d506eb95d04a1e236c4678d
kango31 <cedric.campguilhem@caramail.com>
2016-08-01 14:06:25 +0200

    check ssh agent is working 1

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/**
+ * @brief Main function
+ * 
+ * @param argc number of command line arguments
+ * @param argv command line arguments
+ * @return error code
+ */
 int main(int argc, char* argv[])
 {
     std::cout << "TINY PHYSICS Engine" << std::endl;
